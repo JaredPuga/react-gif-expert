8986172dfc6f93db18e011b6fbf5ed8148758ffc
Jared Puga <jaredpuga6@gmail.com>
2022-12-24 11:55:05 -0600

    First Commit

---
+++ src/hooks/useFetchGifs.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+import { getGifs } from "../helpers/getGifs";
+
+export const useFetchGifs = ( category ) => {
+
+    const [images, setImages] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    
+    const getImagenes = async() =>{
+        const newImagenes = await getGifs( category );
+        setImages(newImagenes);
+        setIsLoading(false)
+    }
+
+    useEffect(() => {
+      getImagenes();
+    }, [ ])
+
+  return {
+    images,
+    isLoading
+  }
+}
